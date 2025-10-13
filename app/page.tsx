ff898ee321c82d917077eb6234c0fd155475162c
Vishesh Gaur <gaurvishesh999@gmail.com>
2025-10-13 22:05:10 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+ <>
+   <p className="text-red-200 text-xl">Hello saas tool</p>
+   <Button variant="ghost"> hello ai saas tool</Button></>
+  );
+}
