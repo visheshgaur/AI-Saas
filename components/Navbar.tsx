93ed36df60c134e4616690b6577e7faab4abc827
Vishesh Gaur <gaurvishesh999@gmail.com>
2025-10-26 00:17:17 +0530

    Fixed hydration error by making UserButton client-only

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,17 +1,36 @@
-import { Ghost, Menu } from "lucide-react";
+// import { Ghost, Menu } from "lucide-react";
+// import { Button } from "./ui/button";
+// import { UserButton } from "@clerk/nextjs";
+// import MobileSidebar from "./Mobile-Sidebar";
+// const Navbar=()=>{
+//     return(
+//         <>
+//        <div className="flex items-center p-4 ">
+//        <MobileSidebar/>
+//         <div className="w-full flex justify-end">
+//             <UserButton afterSignOutUrl="/"/>
+//         </div>
+//        </div>
+//         </>
+//     )
+// }
+// export default Navbar;
+
+import { Menu } from "lucide-react";
 import { Button } from "./ui/button";
-import { UserButton } from "@clerk/nextjs";
 import MobileSidebar from "./Mobile-Sidebar";
-const Navbar=()=>{
-    return(
-        <>
-       <div className="flex items-center p-4 ">
-       <MobileSidebar/>
-        <div className="w-full flex justify-end">
-            <UserButton afterSignOutUrl="/"/>
-        </div>
-       </div>
-        </>
-    )
-}
+import dynamic from "next/dynamic";
+import ClientUserButton from "./ClientUserButton";
+
+const Navbar = () => {
+  return (
+    <div className="flex items-center p-4">
+      <MobileSidebar />
+      <div className="w-full flex justify-end">
+        <ClientUserButton />
+      </div>
+    </div>
+  );
+};
+
 export default Navbar;
